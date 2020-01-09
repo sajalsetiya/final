bd88ed3f011d278ba65217ada48f6d22b3919568
[sajal setiya] <[sajalsetiya1998@gmail.com]>
2020-01-09 23:00:00 +0530

    pp

--- div.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2st number: "))
-
-sub= num1 / num2
-
-print(sub)
--- multi.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2st number: "))
-
-sum= num1 * num2
-
-print(sum)
